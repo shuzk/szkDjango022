d355231207785715414052e42a574357b73558b5
szk_django022 <szk_django022@163.com>
2018-11-06 11:51:58 +0800

    模型——条件查询

--- booktest/models.py
+++ booktest/models.py
@@ -3,17 +3,23 @@ from django.db import models
 
 # 定义图书模型类BookInfo
 class BookInfo(models.Model):
-    btitle = models.CharField(max_length=20)  # 图书名称
-    bpub_date = models.DateField()  # 发布日期
-    bread = models.IntegerField(default=0)  # 阅读量
-    bcomment = models.IntegerField(default=0)  # 评论量
+    # btitle = models.CharField(max_length=20)#图书名称
+    btitle = models.CharField(max_length=20, db_column='title')  # 通过db_column指定btitle对应表格中字段的名字为title
+    bpub_date = models.DateField()
+    # 发布日期
+    bread = models.IntegerField(default=0)
+    # 阅读量
+    bcomment = models.IntegerField(default=0)
+    # 评论量
     isDelete = models.BooleanField(default=False)  # 逻辑删除
 
+    # 定义英雄模型类HeroInfo
 
-# 定义英雄模型类HeroInfo
+
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)  # 英雄姓名
     hgender = models.BooleanField(default=True)  # 英雄性别
     isDelete = models.BooleanField(default=False)  # 逻辑删除
-    hcomment = models.CharField(max_length=200)  # 英雄描述信息
+    # hcomment = models.CharField(max_length=200)#英雄描述信息
+    hcomment = models.CharField(max_length=200, null=True, blank=False)  # hcomment对应的数据库中的字段可以为空，但通过后台管理页面添加英雄信息时hcomment对应的输入框不能为空
     hbook = models.ForeignKey('BookInfo')  # 英雄与图书表的关系为一对多，所以属性定义在英雄模型类中
--- booktest/views.py
+++ booktest/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
 from booktest.models import *
 from datetime import date
-
+from django.db.models import F
+from django.db.models import Q, Sum
 
 # 查询所有图书并显示
 def index(request):
     list = BookInfo.objects.all()
+    list1 = BookInfo.objects.filter(id=1)
+    list2 = BookInfo.objects.filter(btitle__contains='传')
+    list3 = BookInfo.objects.filter(btitle__endswith='部')
+    list4 = BookInfo.objects.filter(btitle__isnull=False)
+    list5 = BookInfo.objects.filter(id__in=[1, 3, 5])
+    list6 = BookInfo.objects.filter(id__gt=3)
+    list7 = BookInfo.objects.exclude(id=3)
+    list8 = BookInfo.objects.filter(bpub_date__year=1980)
+    list9 = BookInfo.objects.filter(bpub_date__gt=date(1990, 1, 1))
+    list10 = BookInfo.objects.filter(bread__gte=F("bcomment"))
+    list11 = BookInfo.objects.filter(bread__gt=F('bcomment') * 2)
+
+    list12 = BookInfo.objects.filter(bread__gt=20, id__lt=3)
+
+    list13 = BookInfo.objects.filter(Q(bread__gt=20) | Q(pk__lt=3))
+    list14 = BookInfo.objects.filter(~Q(pk=3))
+
+    list15 = BookInfo.objects.aggregate(Sum('bread'))
+    list16 = BookInfo.objects.count()
     return render(request, 'booktest/index.html', {'list': list})
 
 
